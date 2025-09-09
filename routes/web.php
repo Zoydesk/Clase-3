a8c922c279c9beaa40c21c4eeebb10efb51f9f15
Raul Beltran <raulb2753@gmail.com>
2025-09-09 14:34:11 -0500

    grupo de rutas

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/products', [ProductController::class, 'index' ]);
 
-Route::get('products/create', [ProductController::class, 'create']);
+Route::prefix('products')->controller(ProductController::class)->group(function () {
 
-Route::get('/products/{id}/{category}', [ProductController::class, 'detail']);
+    Route::get('/', 'index');
 
+    Route::get('/create',  'create');
 
+    Route::get('/{id}/{category}', 'detail');
+});
